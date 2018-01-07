9d03a4d801a3493c62ee6cb857e5955b02ad72fd
Matt Bubernak <matt.bubernak@gmail.com>
2018-01-07 10:10:04 -0700

    add service worker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 import App from './js/components/App';
 ReactDOM.render((<App />), document.getElementById('app'));
+registerServiceWorker();
