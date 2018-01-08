aba2dbfedbadd72db3238a924e107d010d2d8410
Matt Bubernak <matt.bubernak@gmail.com>
2018-01-07 18:34:20 -0700

    move logout to drawer

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -9,6 +9,12 @@ import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
 import LoginLogout from './LoginLogout';
 import UserInfoNav from './UserInfoNav';
+import Drawer from 'material-ui/Drawer';
+import ChevronLeftIcon from 'material-ui-icons/ChevronLeft';
+import ChevronRightIcon from 'material-ui-icons/ChevronRight';
+import Divider from 'material-ui/Divider';
+import List from 'material-ui/List';
+
 
 const styles = theme => ({
   root: {
@@ -24,22 +30,52 @@ const styles = theme => ({
 });
 
 class Nav extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
 
   render() {
     const { classes } = this.props;
     return (
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex}>
-            14ers Checklist
-          </Typography>
-          {this.props.user ? <UserInfoNav user={this.props.user}/> : undefined}
-          <LoginLogout user={this.props.user}/>
-        </Toolbar>
-      </AppBar>
+      <div>
+        <Drawer
+          open={this.state.open}
+          type="persistent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerInner}>
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+            <LoginLogout user={this.props.user}/>
+            <Divider />
+          </div>
+        </Drawer>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} onClick={this.handleDrawerOpen} color="contrast" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography type="title" color="inherit" className={classes.flex}>
+              14ers Checklist
+            </Typography>
+            {this.props.user ? <UserInfoNav user={this.props.user}/> : undefined}
+            {this.props.user ? undefined : <LoginLogout user={this.props.user}/>}
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
